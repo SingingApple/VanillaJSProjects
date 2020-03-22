c457ba1ce85ac7e55a7d6db3dfca731dd770ce71
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-03-23 01:04:30 +0530

    search location added

---
+++ WeatherAPI/scripts/forecast.js
@@ -0,0 +1,18 @@
+const key = "B5nnJAfmOjsELtnYZV2EtcHUjRLXBpFB";
+
+const getCity = async city => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("lucknow")
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
